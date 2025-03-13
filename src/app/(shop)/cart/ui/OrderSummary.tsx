40079ff6b1c6a40f947ed9fd44640e8df08c1aaa
Daniel <daniel.40.pompa@gmail.com>
2025-03-13 23:55:01 +0100

    feat: add OrderSummary component for cart
    Created a new `OrderSummary` component to display a detailed breakdown of the cart's contents, including subtotal, tax, shipping, and total.
    
    - Added `OrderSummary` component to display cart summary information.
    - Integrated `getSummaryInformation` from the cart store to calculate subtotal, tax, and total.
    - Included placeholders for shipping costs (currently set to $0).
    - Added a loading state to handle initial rendering delays.

---
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useCartStore } from '@/store';
+import { formatCurrency } from '@/utils';
+
+export const OrderSummary = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  const { getSummaryInformation } = useCartStore();
+
+  const { itemsInCart, subTotal, tax, total } = getSummaryInformation();
+
+  // Delay initialization until the component is mounted
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) return <p>Loading...</p>;
+
+  return (
+    <>
+      {/* Items in cart */}
+      <div className='flex justify-between mb-4'>
+        <p>Items</p>
+        <p className='font-semibold'>
+          {itemsInCart === 1 ? `${itemsInCart} item` : `${itemsInCart} items`}
+        </p>
+      </div>
+      {/* Subtotal */}
+      <div className='flex justify-between mb-4'>
+        <p>Subtotal</p>
+        <p className='font-semibold'>{formatCurrency(subTotal)}</p>
+      </div>
+      {/* Sale Tax */}
+      <div className='flex justify-between mb-4'>
+        <p>Sale Tax (9%)</p>
+        <p className='font-semibold'>{formatCurrency(tax)}</p>
+      </div>
+      {/* Shipping */}
+      <div className='flex justify-between mb-4'>
+        <p>Shipping</p>
+        <p className='font-semibold'>{formatCurrency(0)}</p>
+      </div>
+      {/* Total */}
+      <div className='flex justify-between border-t border-slate-300 pt-4 mb-6'>
+        <p className='text-lg font-bold'>Total</p>
+        <p className='text-xl font-bold text-slate-800'>{formatCurrency(total)}</p>
+      </div>
+    </>
+  );
+};
